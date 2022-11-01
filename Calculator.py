11234d2385e62e23fcd7e689221f6c7ea8972811
AlexHaavisto <112947422+AlexHaavisto@users.noreply.github.com>
2022-11-01 10:26:06 +0200

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,28 +1,53 @@
 
-print("1 Addition")
-print("2 Subtraction")
-print("3 Multiplicatoin")
-print("4 Division")
+# This function adds two numbers
+def add(x, y):
+    return x + y
 
-choice = input("Enter your choice : ")
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
 
-num1 = float(input("Enter Number 1 : "))
-num2 = float(input("Enter Numero 2 : "))
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
 
-if choice == "1":
-    print(num1, "+", num2, "=", (num1+num2))
+# This function divides two numbers
+def divide(x, y):
+    return x / y
 
-elif choice == "2":
-    print(num1, "-", num2, "=", (num1-num2))
 
-elif choice == "3":
-    print(num1, "*", num2, "=", (num1*num2))
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
 
-elif choice == "4":
-    if num2 == 0.0:
-        print("Divide by 0 Error")
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    
     else:
-        print(num1, "/", num2, "=", (num1/num2))
-
-else:
-    print("invalic choice")
+        print("Invalid Input")
