6c6e92a662edb6df2a56f763916f041490307d37
AlexHaavisto <112947422+AlexHaavisto@users.noreply.github.com>
2022-11-02 11:53:22 +0200

    Delete tavupermutaatio.py

--- tavupermutaatio.py
+++
@@ -1,9 +0,0 @@
-import itertools
-#print (list(itertools.permutations(["Au","sa","Koi","ra"], 2)))
-# tulos = (list(itertools.combinations(["gan","da","mel","zap","cra"], 3)))
-# print(tulos)
-
-tulos = (list(itertools.product(["Au","sa","Koi","ra"], ["nen","la","ras","muu"], ["si","la","ka","na"])))
-
-for sana in tulos:
-    print(sana[0] + sana[1] + sana[2])
